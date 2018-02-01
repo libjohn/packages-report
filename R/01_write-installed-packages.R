3a0e1afcc0bf91b31019fdb50d6a745d1431ece8
libjohn <john.little@duke.edu>
2018-02-01 11:41:22 -0800

    new script writes data
    - uses library(here)

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,20 @@
 ## deja vu from yesterday!
 
+## Libraries
+## let's confirm the second element is, in fact, the default library
+library(fs)
+library(here)
+
 ## create a data frame of your installed packages
+
+#' Installed packages
+
+ipkgs <- installed.packages() %>%
+  as_tibble()
+
+ipkgs
+
+
 
 ## keep the variables
 ##   * Package
@@ -9,10 +23,21 @@
 ##   * Priority
 ##   * Built
 
+small_ipkgs <- ipkgs %>%
+  select(Package, LibPath, Version, Priority, Built)
+
+small_ipkgs
+
+## Get Jiggy with library(here)
+
+here("data", "foo.csv")
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+write_csv(small_ipkgs, here("data", "installed-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
